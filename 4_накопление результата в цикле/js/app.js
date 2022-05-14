d6d438ae4762b289ce79d65d384da249660befe5
Mershana <102474997+Mershana@users.noreply.github.com>
2022-05-14 14:44:15 +0300

    cycles_js

---
+++ 4_накопление результата в цикле/js/app.js
@@ -0,0 +1,26 @@
+'use strict' //строгий режим
+
+// Накопление результата в цикле JavaScript
+// Задача 1
+// Найдите произведение целых чисел от 1 до 20.
+// let result = 1;
+// for (let i = 1; i <= 20; i++){
+// result = result * i;
+// }
+// console.log(result) // 2432902008176640000
+
+// Задача 2
+// Найдите сумму четных чисел от 2 до 100.
+// let result = 0;
+// for (let i = 2; i <= 100; i+=2){
+// 	result = result + i;
+// }
+// console.log(result) // 2550
+
+// Задача 3
+// Найдите сумму нечетных чисел от 1 до 99.
+// let result = 0;
+// for (let i = 1; i <= 99; i+=2){
+// 	result = result + i;
+// } 
+// console.log(result) //2501
